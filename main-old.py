dff6ed0c709d8e7228d548bdb1d25fdd7d77c847
Matthew Farrugia-Roberts <m@far.in.net>
2023-02-21 22:09:56 +1100

    refresh podcast downloader script (todo ffmpeg integration)

--- main-old.py
+++ main-old.py
@@ -1,51 +1,85 @@
+#!python3
+
 import io
 import os
+import sys
 import datetime
 import mimetypes
 
-import tomli as tomllib # TODO replace with tomllib in python 3.11+
-import tqdm
+import rtoml as toml
 import requests
 import podcastparser
+import tqdm
 import filename_sanitizer as sani
 
-
-RSS_LIST  = "./feeds.toml"
-ROOT_PATH = "./downloads/"
+CONFIG_PATH = "/Users/matt/.feeds"
 
 
 def main():
-    with open(RSS_LIST, 'rb') as rss_list_path:
-        feeds = tomllib.load(rss_list_path)
-    for folder, podcasts in feeds.items():
-        for podcast, rss_url in podcasts.items():
-            print("PODCAST", podcast)
-            path = os.path.join(ROOT_PATH, folder, podcast)
-            update_feed(rss_url, path)
+    # load config
+    with open(CONFIG_PATH) as config_file:
+        config = toml.load(config_file)
 
+    # process each feed
+    for feed_id, feed_data in config.items():
+        update_feed(feed_id, **feed_data)
 
-def update_feed(rss_url, path):
-    # setup folder
+def update_feed(feed_id, url, path):
+    print("* updating", feed_id)
+
+    # set up directory
+    print(" setting up path", path)
     os.makedirs(path, exist_ok=True)
-    # download and parse rss
-    rss_stream = io.BytesIO(requests.get(rss_url).content)
-    rss = podcastparser.parse(rss_url, rss_stream)
-    # download all episodes
-    episodes = rss['episodes']
-    for episode in episodes[::-1]:
-        download_episode(episode, path)
 
+    # download rss file
+    print(" downloading rss file from", url)
+    rss_path = os.path.join(path, "rss.xml")
+    download_rss(url, rss_path)
+
+    # parse rss file
+    print(" parsing rss file")
+    rss_feed = parse_rss(url, rss_path)
+
+    # title = rss_feed['title']
+    # description = rss_feed['description']
+    episodes = rss_feed['episodes']
+    print(" found", len(episodes), "episodes")
+    for episode in episodes[::-1]:
+        # try:
+            download_episode(episode, path)
+        # except:
+        #     with open("errors.log", 'a') as f:
+        #         print("ERROR", episode, file=f, flush=True)
+        #         print("ERROR", episode, file=sys.stderr)
+        #         # TODO: handle errors better (a summary at the end of
+        #         # update, for example?)
+
+    print("", feed_id, "up to date")
+
+
+def download_rss(url, path):
+    r = requests.get(url)
+    r.raise_for_status()
+    # (save rss file?)
+    with open(path, 'wb') as f:
+        f.write(r.content)
+    
+def parse_rss(url, rss_path):
+    with open(rss_path) as f:
+        return podcastparser.parse(url, f)
 
 def download_episode(episode, folder_path):
-    title   = episode['title']
+    # episode details
+    title = episode['title']
     pubdate = datetime.datetime.fromtimestamp(episode['published'])
+    # others: subtitle, description
 
     # episode file
     if episode['enclosures'] == []:
-        print("WARNING: Missing episode file!")
+        tqdm.tqdm.write("WARNING: Missing episode file!")
         return
     if len(episode['enclosures']) > 1:
-        print("WARNING: Multiple episode files! (taking first)")
+        tqdm.tqdm.write("WARNING: Multiple episode files! (taking first)")
     file = episode['enclosures'][0];
     mimetype = file['mime_type']
     file_url = file['url']
@@ -78,8 +112,9 @@ def download_episode(episode, folder_path):
                     f.write(chunk)
         progress.close()
 
-
-# mimetypes
+# # #
+# Translating mimetypes
+# 
 mimetypes.add_type("audio/x-m4a", ".m4a", strict=False)
 def guess_extension(mimetype):
     dot_ext = mimetypes.guess_extension(mimetype, strict=False)
